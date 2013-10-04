c8b9f0ee74aa92c23ceb1775c74d084f54a566fd
brianhouse <brian.house@gmail.com>
2013-10-04 15:22:13 -0400

    integrating parsing of improved template

--- main_twitter.py
+++ main_twitter.py
@@ -13,8 +13,20 @@ while True:
     log.info("%s recent clips" % len(clips))
     for clip in clips:
         log.info("Checking %s %s %s" % (clip['t'], clip['hit_id'], util.datestring(clip['t'])))
-        text = mturk.retrieve_result(clip['hit_id'])        
-        if text is not None:
-            model.mark_clip(clip['t'])
-            ts.queue.put(text)
+        struct = mturk.retrieve_result(clip['hit_id'])   
+        if struct is None:
+            continue
+        model.mark_clip(clip['t'])                        
+        if 'nospeech' in struct and struct['nospeech'] == 'on':
+            log.info("--> no speech in clip")
+            continue
+        try:
+            for label in ('line_1', 'line_2', 'line_3'):
+                if label in struct and len(struct[label]):
+                    message = '"%s"' % struct[label]
+                    ts.queue.put(message)
+        except Exception as e:
+            log.error(log.exc(e))
+            continue        
     time.sleep(30)
+
